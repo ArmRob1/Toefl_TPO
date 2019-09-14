02d79d1f4c08198d4218e8bb7a991a73465b6113
Home <Home@LAPTOP-N601TTK4>
2019-09-15 01:23:56 +0400

    Again

---
+++ TPO/Toefl_TPO/Pages/EditPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Toefl_TPO.Pages
+{
+    /// <summary>
+    /// Interaction logic for EditPage.xaml
+    /// </summary>
+    public partial class EditPage : Page
+    {
+        public EditPage()
+        {
+            InitializeComponent();
+        }
+
+        private void File_Click(object sender, RoutedEventArgs e)
+        {
+            //OpenFileDialog ofd = new OpenFileDialog();
+
+
+
+            //ofd.InitialDirectory = "c:\\";
+            //ofd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            //ofd.FilterIndex = 2;
+            //ofd.RestoreDirectory = true;
+
+            //if (ofd.ShowDialog() == DialogResult.OK)
+            //{
+            //    filePath = ofd.FileName;
+
+            //    var fileStream = ofd.OpenFile();
+
+            //    using (StreamReader reader = new StreamReader(fileStream))
+            //    {
+            //        fileContent = reader.ReadToEnd();
+            //    }
+            //}
+
+        }
+    }
+}
